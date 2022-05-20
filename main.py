0141d0e66ecf1bc1fc8fa31463169abc6b529533
Yuzuki Arai <yudukikun5120@gmail.com>
2022-05-21 04:23:10 +0900

    Change to response the JSON object directly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """Module Main serving problems as an API."""
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 import pandas as pd
 
 app = FastAPI()
@@ -10,7 +10,8 @@ def read_items(category: str):
     "Function read items."
     problems = pd.read_pickle(F'problems/{category}.pkl')
 
-    return problems.to_json(orient='records', force_ascii=False, lines=True)
+    return Response(problems.to_json(orient='records', force_ascii=False,
+                    lines=True), media_type="application/json")
 
 
 @app.get("/items/{category}/{id_}")
@@ -19,4 +20,9 @@ def read_item(category: str, id_: int):
     problems = pd.read_pickle(F'problems/{category}.pkl')
     problem = problems.iloc[id_]
 
-    return problem.to_json(force_ascii=False)
+    return Response(problem.to_json(force_ascii=False),
+                    media_type="application/json")
+
+
+if __name__ == '__main__':
+    print(read_item("onyomi", 1))
