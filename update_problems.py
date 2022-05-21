0f15fcdaf3d9c698cb7d1909c89ba65618bbdd2d
Yuzuki Arai <yudukikun5120@gmail.com>
2022-05-22 00:44:27 +0900

    Fix types of functions

--- update_problems.py
+++ update_problems.py
@@ -136,32 +136,40 @@ converters = {
 }
 
 
-def fetch_problems(*, category, attr):
-    "Function fetching problems."
+def fetch_problems(*, category: str, attr: dict) -> pd.DataFrame:
+    """Function fetching problems."""
     try:
         uri = F'http://teihitsu.html.xdomain.jp/{category}.xls'
-
         print(F"Fetching problems from {uri}...")
-
         dataframe = pd.read_excel(uri,
                                   index_col=attr["index_col"],
                                   header=attr["header"],
                                   names=attr["names"],
                                   converters=converters)
 
-        print(dataframe.to_json(orient='records', force_ascii=False,
-                                lines=True))
+        return dataframe
 
-        dataframe.to_pickle(F'problems/{category}.pkl')
     except Exception as error:
         raise error
 
 
-def main():
-    "Function fetching problems for all categories."
-    for category, attr in categories.items():
-        fetch_problems(category=category, attr=attr)
+def dataframe_to_json(dataframe: pd.DataFrame) -> dict:
+    """Function converting the DataFrame to JSON format."""
+    return dataframe.to_json(orient='records', force_ascii=False, lines=True)
+
+
+def store_dataframe(*, dataframe: pd.DataFrame, category: str) -> None:
+    """Function storing the DataFrame in pickle format."""
+    dataframe.to_pickle(F'problems/{category}.pkl')
+
+
+def main(categories_: dict) -> None:
+    """Function fetching problems for all categories."""
+    for category, attr in categories_.items():
+        dataframe = fetch_problems(category=category, attr=attr)
+        store_dataframe(dataframe=dataframe, category=category)
+        print(dataframe_to_json(dataframe))
 
 
 if __name__ == '__main__':
-    main()
+    main(categories)
