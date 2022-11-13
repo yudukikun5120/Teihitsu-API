bc24f2fba4a98bf7da1e74a15b899a24717267b3
Yuzuki Arai <yudukikun5120@gmail.com>
2022-11-14 03:17:01 +0900

    test: fix the NaN value

--- test_problems_dataframe.py
+++ test_problems_dataframe.py
@@ -1,7 +1,6 @@
 """Module _Unittest testing module update_problems"""
 import json
 import unittest
-import numpy as np
 from update_problems import fetch_problems
 
 
@@ -19,7 +18,7 @@ class TestProblemsDataFrame(unittest.TestCase):
         self.assertEqual(dataframe.at[1, 'problem'], "蹉跌")
         self.assertEqual(dataframe.at[1, 'level'], 1)
         self.assertEqual(dataframe.at[1, 'correct_answer'], 'さてつ')
-        self.assertTrue(np.isnan(dataframe.at[1, 'alt_correct_answers']))
+        self.assertEqual(dataframe.at[1, 'alt_correct_answers'], 0)
         self.assertEqual(dataframe.at[1, 'note'], "┌1.つまずくこと。\n"
             "└2.障害や失敗があって、行き詰まること。≒挫折〈漢字ペディア〉\n┌物事がうまく進まず、しくじること。挫折。失敗。〈大辞泉〉\n"
             "┌1.つまずいて足をすべらす。\n└2.ちぐはぐになって、失敗する。〈漢字源〉\n◇漢字ペディア大見出し\n●H4-1㈠"
